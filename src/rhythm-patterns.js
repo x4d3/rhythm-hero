896281d40aa91ce882ffe51a2de85a7e332844f5
x4d3 <git@xade.eu>
2015-10-27 18:04:18 +0000

    start RhythmPatterns implementation

--- src/rhythm-patterns.js
+++ src/rhythm-patterns.js
@@ -2,6 +2,34 @@ RH.RhythmPatterns = (function(){
 	'use strict';
 	var RhythmPatterns = {};
 	
+	var Preconditions = RH.Preconditions;
+	
+	
+	var Pattern = function(description, difficulty, frequency, notes){
+		this.description = Preconditions.checkIsString(description);
+		this.difficulty = Preconditions.checkIsNumber(difficulty);
+		this.frequency = Preconditions.checkIsNumber(frequency);
+		this.notes = notes;
+	};
+	
+	var Note = function(duration, isRest){
+		this.duration = duration;
+		this.isRest = isRest;
+	};
+	
+	var PATTERNS = [];
+	var n = function(numerator, denominator, isRest){
+		return new Note(new Fraction(numerator, denominator), isRest === undefined? false : isRest);
+	};
+	
+	var addPattern = function(description, difficulty, frequency, notes){
+		var pattern = new Pattern(description, difficulty, frequency, notes);
+		PATTERNS.push(pattern);
+	};
+	
+	addPattern("one crotchet", 0, 100, n(1,1));
+	
+	
 	return RhythmPatterns;
 });
 
