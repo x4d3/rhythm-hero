a522eb7b80b4a87580ddf945d341fb5003b0ff4f
x4d3 <git@xade.eu>
2015-10-29 00:00:10 +0100

    implemented toString in pattern, small bug correction

--- src/rhythm-patterns.js
+++ src/rhythm-patterns.js
@@ -4,6 +4,15 @@ RH.RhythmPatterns = (function(){
 	
 	var Preconditions = RH.Preconditions;
 	
+	var Note = function(duration, isRest){
+		this.duration = duration;
+		this.isRest = isRest;
+	};
+	Note.prototype = {
+		toString:function(){
+			return this.duration.toFraction() + (this.isRest ? "r" : "");
+		}
+	};
 	
 	var Pattern = function(description, difficulty, frequency, notes){
 		this.description = Preconditions.checkIsString(description);
@@ -11,22 +20,30 @@ RH.RhythmPatterns = (function(){
 		this.frequency = Preconditions.checkIsNumber(frequency);
 		this.notes = notes;
 	};
+	
 	Pattern.prototype = {
 		getDuration:function(){
-			return this.notes.reduce(function(sum, note){return sum.add(note.duration);});
+			return this.notes.reduce(function(sum, note){
+				return sum.add(note.duration);
+			}, new Fraction(0));
+		},
+		toString:function(){
+			return "Pattern[" + this.description +
+			",D:" + this.difficulty +
+			",F:" + this.frequency +
+			",notes:" + this.notes +
+			",duration:" + this.getDuration().toFraction() + "]";
 		}
 	};
-	var Note = function(duration, isRest){
-		this.duration = duration;
-		this.isRest = isRest;
-	};
 	
+
 	var parseNotes = function(value){
 		var notes = [];
 		var split = value.split(",");
 		for (var i = 0; i < split.length; i++) {
 			notes[i] = parseNote(split[i].trim());
 		}
+		return notes;
 	};
 	
 	var parseNote = function(value){
@@ -56,22 +73,21 @@ RH.RhythmPatterns = (function(){
 	addPattern("one whole", 0, 10, "4/1");
 	addPattern("one minim", 0, 10, "2/1");
 	addPattern("one crotchet", 0, 100, "1/1");
-	addPattern("2 quivers", 1, 100, "1/2,1/2");
-	addPattern("dotted crotchet quiver", 1, 100, "3/2,1/2");
-	addPattern("dotted crotchet quiver", 1, 100, "1/2,3/2");
+	addPattern("double quivers", 1, 100, "1/2,1/2");
+	addPattern("dotted, crotchet quaver", 1, 100, "3/2,1/2");
+	addPattern("quaver , dotted crotchet", 1, 100, "1/2,3/2");
 	
-	addPattern("triplet", 2, 20, "1/3,1/3,1/3");
-	addPattern("crotchet triplet", 3, 20, "2/3,2/3,2/3");
+	addPattern("triplet quaver ", 2, 20, "1/3,1/3,1/3");
+	addPattern("triplet crotchet", 3, 20, "2/3,2/3,2/3");
 	
 	addPattern("crotchet rest", 0, 100, "1/1r");
 	addPattern("crotchet rest", 1, 100, "1/2,1/2r");
-	addPattern("crotchet rest", 1, 100, "1/2r,1/2");
+	addPattern("rest crotchet", 1, 100, "1/2r,1/2");
 	
 	var difficulties = PATTERNS.map(function(x){return x.difficulty;});
-	
 	RhythmPatterns.MAX_DIFFICULTY = Math.max.apply(Math, difficulties);
 
-	RhythmPatterns.generatePattern = function(minDifficulty, maxDifficulty, size){
+	RhythmPatterns.generatePatterns = function(minDifficulty, maxDifficulty, size){
 		var filtered = PATTERNS.filter(function(x){return x.difficulty >= minDifficulty && x.difficulty<=maxDifficulty;});
 		var sumFrequency = 0;
 		var summedFrequencies = filtered.map(function(x){
@@ -85,8 +101,7 @@ RH.RhythmPatterns = (function(){
 		}
 		return result;
 	};
-	
-	
+	RhythmPatterns.PATTERNS = PATTERNS;
 	return RhythmPatterns;
 })();
 
--- tests/rhythm-patterns-test.js
+++ tests/rhythm-patterns-test.js
@@ -2,16 +2,18 @@ module("RhythmPatterns");
 
 test("general", function() {
 	RH.Preconditions.checkIsNumber(RH.RhythmPatterns.MAX_DIFFICULTY);
-	expect(0);
+	var patterns = RH.RhythmPatterns.PATTERNS;
+	for (var i = 0; i < patterns.length; i++) {
+		console.log(patterns[i]);
+	}
+	equal(RH.RhythmPatterns.MAX_DIFFICULTY , 3);
+	
 });
 
 
 test("generatePattern", function() {
 
-	var patterns = RH.RhythmPatterns.generatePattern(0, RH.RhythmPatterns.MAX_DIFFICULTY, 10);
+	var patterns = RH.RhythmPatterns.generatePatterns(0, RH.RhythmPatterns.MAX_DIFFICULTY, 10);
 	equal(patterns.length, 10);
-	for (var i = 0; i < patterns.length; i++) {
-		console.log(patterns[i].description);
-	}
 });
 
