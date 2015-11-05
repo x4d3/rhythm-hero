bd47b919b9a315624be2b4af1c4a51b388857a84
x4d3 <git@xade.eu>
2015-11-05 14:44:14 +0000

    move measure to a new file

--- src/game.js
+++ src/game.js
@@ -1,19 +1,3 @@
-RH.Measure = (function(){
-	'use strict';
-	var Measure = function(notes, firstNotePressed, lastNotePressed){
-		this.notes = notes;
-		this.firstNotePressed = firstNotePressed;
-		this.lastNotePressed = lastNotePressed;
-	};
-	
-	Measure.prototype = {
-		toString : function(){
-			return "{" + this.notes + ", " + this.firstNotePressed + ", " + this.lastNotePressed + "}";
-		}
-	};
-	return Measure;
-}());
-
 RH.GameOptions = (function(){
 	'use strict';
 	function GameOptions(debugMode, timeSignature, tempo){
---
+++ src/measure.js
@@ -0,0 +1,15 @@
+RH.Measure = (function(){
+	'use strict';
+	var Measure = function(notes, firstNotePressed, lastNotePressed){
+		this.notes = notes;
+		this.firstNotePressed = firstNotePressed;
+		this.lastNotePressed = lastNotePressed;
+	};
+	
+	Measure.prototype = {
+		toString : function(){
+			return "{" + this.notes + ", " + this.firstNotePressed + ", " + this.lastNotePressed + "}";
+		}
+	};
+	return Measure;
+}());
